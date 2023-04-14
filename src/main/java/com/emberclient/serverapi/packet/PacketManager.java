3c32e2ee9d4226d23e7b99c4954678343bafa38d
marvintheskid <46824675+marvintheskid@users.noreply.github.com>
2023-04-14 13:24:44 +0200

    fix: PacketManager#getPacketId is now O(1) instead of O(n)

--- src/main/java/com/emberclient/serverapi/packet/PacketManager.java
+++ src/main/java/com/emberclient/serverapi/packet/PacketManager.java
@@ -5,13 +5,13 @@ import com.emberclient.serverapi.packet.impl.attestation.register.InAttestationR
 import com.emberclient.serverapi.packet.impl.attestation.register.OutAttestationRegister;
 import com.emberclient.serverapi.packet.impl.attestation.sign.InAttestationSign;
 import com.emberclient.serverapi.packet.impl.attestation.sign.OutAttestationSign;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import io.netty.buffer.Unpooled;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-
 public class PacketManager {
-    private HashMap<Integer, Class<? extends Packet>> packets = new HashMap<>();
+    private final BiMap<Integer, Class<? extends Packet>> packets = HashBiMap.create();
 
     public PacketManager() {
         packets.put(1, OutAttestationRegister.class);
@@ -22,20 +22,17 @@ public class PacketManager {
     }
 
     private int getPacketId(Packet packet) {
-        for (int id : packets.keySet()) {
-            if (packets.get(id).equals(packet.getClass())) {
-                return id;
-            }
-        }
-        return -1;
+        return packets.inverse().get(packet.getClass());
     }
 
     public void handle(Player player, ByteBufWrapper buf) {
         int id = buf.readVarInt();
         Class<? extends Packet> packetClass = packets.get(id);
+
         if (packetClass == null) {
             return;
         }
+
         try {
             Packet packet = packetClass.newInstance();
 
